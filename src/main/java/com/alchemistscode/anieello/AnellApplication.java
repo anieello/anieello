23007b2ce372d9fbd78adb7fda9f204c0a91a86e
Ricardo Morales Paredes <inkcannon@gmail.com>
2021-03-06 00:27:23 -0600

    Add java 11 / Spring Skellington
    * Add The basic structure of java 
    * Add The basic structure to spring boot 
    * Add maven support

---
+++ src/main/java/com/alchemistscode/anieello/AnellApplication.java
@@ -0,0 +1,13 @@
+package com.alchemistscode.anieello;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnellApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnellApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/alchemistscode/anieello/AnellApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alchemistscode.anieello;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnellApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
